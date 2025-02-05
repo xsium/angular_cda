ed7b420c47f9d5522c168e1d72a14ee743b1e0ca
xsium <simon.metayerlr@gmail.com>
2025-02-05 14:00:53 +0100

    tp blog beautify

--- src/app/add-article/add-article.component.ts
+++ src/app/add-article/add-article.component.ts
@@ -12,22 +12,18 @@ export class AddArticleComponent {
   title: string = "";
   content: string ="";
   author: string ="";
-  date: Date = this.getDate()
 
-  getDate() :Date {
-    return new Date();
-  }
 
     
   @Output() sendArticle = new EventEmitter< { title: string; content: string; author: string; date: Date }>();
   @Output() sendBrouillon = new EventEmitter< { title: string; content: string; author: string; date: Date }>();
 
   addArticle() {
-    const article: { title: string; content: string; author: string; date: Date }= {title:this.title, content:this.content, author:this.author, date:this.date};
+    const article: { title: string; content: string; author: string; date: Date }= {title:this.title, content:this.content, author:this.author, date: new Date()};
     this.sendArticle.emit(article);
   }
   addBrouillon(){
-    const article: { title: string; content: string; author: string; date: Date }= {title:this.title, content:this.content, author:this.author, date:this.date};
+    const article: { title: string; content: string; author: string; date: Date }= {title:this.title, content:this.content, author:this.author, date: new Date()};
     this.sendBrouillon.emit(article);
   }
 }
